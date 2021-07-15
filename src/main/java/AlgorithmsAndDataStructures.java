c6ae4383fb04e07726ce93d784b5101cafe67c20
bohdan.karanchuk <b.karanchuk@gmail.com>
2021-07-15 10:51:57 +0300

    Выполнены все задачи урока

--- src/main/java/AlgorithmsAndDataStructures.java
+++ src/main/java/AlgorithmsAndDataStructures.java
@@ -600,6 +600,62 @@ public class AlgorithmsAndDataStructures {
          * поддеревьях различается не более чем на 1.
          */
 
+        /**
+         * Задание 7.1
+         * Приведите пример графа.
+         *
+         * Примером графа может быть сеть маршрутов с расстояниями между его вершинами – городами.
+         */
+
+        /**
+         * Задание 7.2
+         * Реализуйте базовые методы графа.
+         */
+        System.out.println("\nЗадание 7.2");
+        Graph graph = new Graph();
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+        graph.addVertex('E');
+        graph.addVertex('H');
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(3, 4);
+
+        graph.displayVertex(4);
+
+        /**
+         * Задание 7.3
+         * В программный код из задания 7.2 добавьте реализацию метода обхода
+         * в глубину.
+         * Выполните оценку времени с помощью System.nanoTime().
+         */
+        System.out.println("\nЗадание 7.3");
+        timer.start();
+        graph.dfs();
+        System.out.println("Обход в глубину занимает " + timer.fix() + " наносекунд");
+
+        timer.start();
+        graph.DFS(0);
+        System.out.println("Обход в глубину с рекурсией занимает " + timer.fix() + " наносекунд");
+
+        /**
+         * Задание 7.4
+         * В базовом графе из задания 7.2 реализуйте метод обхода в ширину.
+         * Выполните оценку времени с помощью System.nanoTime().
+         */
+        System.out.println("\nЗадание 7.4");
+        timer.start();
+        graph.bfs();
+        System.out.println("Обход в ширину занимает " + timer.fix() + " наносекунд");
+
+        timer.start();
+        graph.BFS(0);
+        System.out.println("Обход 2 в ширину занимает " + timer.fix() + " наносекунд");
+
     }
 
     public static class NewType {
@@ -1278,4 +1334,168 @@ public class AlgorithmsAndDataStructures {
             a[j] = temp;
         }
     }
+
+    static class Vertex {
+        char label;
+        boolean wasVisited;
+
+        public Vertex(char label) {
+            this.label = label;
+            this.wasVisited = false;
+        }
+    }
+
+    static class Graph {
+        private final int MAX_VERTS = 32;
+        private Vertex[] vertexList;
+        private int[][] adjMat;
+        private int size;
+        private Stack stack;
+
+        public Graph() {
+            stack = new Stack(MAX_VERTS);
+            vertexList = new Vertex[MAX_VERTS];
+            adjMat = new int[MAX_VERTS][MAX_VERTS];
+            size = 0;
+            for (int i = 0; i < MAX_VERTS; i++) {
+                for (int j = 0; j < MAX_VERTS; j++) {
+                    adjMat[i][j] = 0;
+                }
+            }
+        }
+
+        private int getAdjUnvisitedVertex(int ver) {
+            for (int i = 0; i < size; i++) {
+                if (adjMat[ver][i] == 1 && vertexList[i].wasVisited == false)
+                    return i;
+            }
+            return -1;
+        }
+
+        public void dfs() {
+            vertexList[0].wasVisited = true;
+            displayVertex(0);
+            stack.push(0);
+            while (!stack.isEmpty()) {
+                int v = getAdjUnvisitedVertex(stack.peek());
+                if (v == -1)
+                    stack.pop();
+                else {
+                    vertexList[v].wasVisited = true;
+                    displayVertex(v);
+                    stack.push(v);
+                }
+            }
+
+            for (int i = 0; i < size; i++) {
+                vertexList[i].wasVisited = false;
+            }
+        }
+
+        public void DFS(int f) {
+            vertexList[f].wasVisited = true;
+
+            for (int i = 0; i < size; i++) {
+                int v = getAdjUnvisitedVertex(f);
+                if (!vertexList[i].wasVisited && v != -1) {
+                    fullDisplayVertex(f, v);
+                    DFS(i);
+                }
+            }
+        }
+
+        public void bfs() {
+            Queue<Integer> queue = new LinkedList<>();
+            vertexList[0].wasVisited = true;
+            displayVertex(0);
+            queue.add(0);
+            int v2;
+            while (!queue.isEmpty()) {
+                int v1 = queue.remove();
+                while ((v2 = getAdjUnvisitedVertex(v1)) != -1) {
+                    vertexList[v2].wasVisited = true;
+                    displayVertex(v2);
+                    queue.add(v2);
+                }
+            }
+            for (int i = 0; i < size; i++) {
+                vertexList[i].wasVisited = false;
+            }
+        }
+
+        public void BFS(int v) {
+            int[] queue = new int[size];
+            int qH = 0;
+            int qT = 0;
+
+            vertexList[0].wasVisited = true;
+            queue[qT++] = v;
+
+            displayVertex(0);
+
+            int v2;
+            while (qH < qT) {
+                v = queue[qH++];
+
+                for (int i = 0; i < size; i++) {
+                    v2 = getAdjUnvisitedVertex(v);
+                    if (!vertexList[i].wasVisited && v2 != -1) {
+                        vertexList[i].wasVisited = true;
+                        displayVertex(qT);
+                        queue[qT++] = i;
+                    }
+                }
+            }
+            for (int i = 0; i < size; i++) {
+                vertexList[i].wasVisited = false;
+            }
+        }
+
+        public void addVertex(char label) {
+            vertexList[size++] = new Vertex(label);
+        }
+
+        public void addEdge(int start, int end) {
+            adjMat[start][end] = 1;
+            adjMat[end][start] = 1;
+        }
+
+        public void displayVertex(int vertex) {
+            System.out.println(vertexList[vertex].label);
+        }
+
+        public void fullDisplayVertex(int vertex1, int vertex2) {
+            System.out.println("Вершины " + vertexList[vertex1].label + "-" + vertexList[vertex2].label);
+        }
+    }
+
+    public static class Stack {
+        private int maxSize;
+        private int[] stackArr;
+        private int top;
+
+        public Stack(int size) {
+            this.maxSize = size;
+            this.stackArr = new int[size];
+            this.top = -1;
+        }
+
+        public void push(int i) {
+            stackArr[++top] = i;
+        }
+
+        public int pop() {
+            return stackArr[top--];
+        }
+
+        public boolean isEmpty() {
+            return (top == -1);
+        }
+
+        public int peek() {
+            return stackArr[top];
+        }
+
+    }
+
 }
